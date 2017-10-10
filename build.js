7a7138272cf49580d8e1da3ac9a5d16b6d47d47c
Noah Muth <noahmuth@gmail.com>
2017-10-10 12:18:23 +0100

    Generate script from shrekfile

---
+++ build.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const path = require('path')
+const shrekFile = path.resolve(__dirname, './shrek')
+const shrek = fs.readFileSync(shrekFile, 'utf8')
+const safeShrek = shrek.replace(/(`|\\)/g, '\\$1')
+
+const binFile = path.resolve(__dirname, './bin/git-shrekt.js')
+fs.writeFileSync(binFile, `
+#!/usr/bin/env node
+
+console.info(\`
+${safeShrek}
+\`)
+`)
